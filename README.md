a3b73dfa7b4fbfec3a227d374e7f5b70a37dc5e4
juancarlospaco <juancarlospaco@gmail.com>
2020-01-02 21:49:24 -0300

    Fix for Nim 1.0+

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Nim WebAssembly Example
+
 ```sh
 git clone https://github.com/2vg/nim-wasm-example
 cd nim-wasm-example
@@ -5,3 +7,8 @@ nimble installEmcc
 nimble buildwasm
 nimble execwasm
 ```
+
+
+# Requirements
+
+- Java (for the Closure Compiler, can be OpenJDK Headless at least).
--- src/nim_wasm_example.nim
+++ src/nim_wasm_example.nim
@@ -1,9 +1,6 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
-
 import macros
 
-macro EMSCRIPTEN_KEEPALIVE*(someProc: untyped): typed =
+macro EMSCRIPTEN_KEEPALIVE(someProc: untyped) =
   result = someProc
   result.addPragma(newIdentNode("exportc"))
   when defined(cpp):
@@ -15,5 +12,5 @@ macro EMSCRIPTEN_KEEPALIVE*(someProc: untyped): typed =
       newIdentNode("codegenDecl"),
       newLit("__attribute__((used)) $# $#$#")))
 
-proc fromNimFunction: int {.EMSCRIPTEN_KEEPALIVE.} =
-  return 114514
+func fromNimFunction: int {.EMSCRIPTEN_KEEPALIVE.} =
+  result = 114514
